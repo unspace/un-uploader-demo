50ce5f46a806b029bbbebde55c772b3e2bcaf813
Carsten Nielsen <heycarsten@gmail.com>
2014-05-26 10:46:38 -0400

    Include Sidekiq test helper.

--- server/spec/spec_helper.rb
+++ server/spec/spec_helper.rb
@@ -1,8 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'sidekiq/testing'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -28,4 +29,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'random'
+
+  config.before(:each) do
+    Sidekiq::Worker.clear_all
+  end
 end
