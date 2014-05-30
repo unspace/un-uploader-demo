edab648abe980a02e6ea2072649f852fcfc76b01
Carsten Nielsen <heycarsten@gmail.com>
2014-05-29 22:22:58 -0400

    Initialize Pusher.

--- server/config/initializers/upload-server.rb
+++ server/config/initializers/upload-server.rb
@@ -1,4 +1,16 @@
+secrets = Rails.application.secrets
+
 # Ensure Redis is running
-$redis = Redis.new(url: Rails.application.secrets.redis)
+$redis = Redis.new(url: secrets.redis)
 $redis.ping
 
+
+Pusher.logger = Rails.logger
+
+Pusher.url = begin
+  key    = secrets.pusher_key
+  secret = secrets.pusher_secret
+  app_id = secrets.pusher_app_id
+
+  "http://#{key}:#{secret}@api.pusherapp.com/apps/#{app_id}"
+end
