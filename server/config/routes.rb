51bde07781d44985ce3d1e833b1bd238cf01f875
Carsten Nielsen <heycarsten@gmail.com>
2014-05-29 22:22:00 -0400

    Add naming to v1 routes.
    Then respond_with works as expected.

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
-    scope module: :v1, constraints: UploadServer::ApiConstraint.new(version: 1, default: true) do
+    namespace :v1, path: '/', constraints: UploadServer::ApiConstraint.new(version: 1, default: true) do
       resources :images,  only: [:index, :show, :create]
       resources :uploads, only: [:create]
     end
