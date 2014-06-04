16526e7f2b1b79a2851799bab3f81af759b83544
Mattia Gheda <ghedamat@gmail.com>
2014-06-04 13:17:52 -0400

    first basic test

---
+++ client/tests/acceptance/images-test.js
@@ -0,0 +1,28 @@
+import startApp from '../helpers/start-app';
+import { defineFixture } from 'ic-ajax';
+
+defineFixture('/api/images', {
+  response: {"images": []},
+  textStatus: 'success'
+});
+
+var App;
+
+var indexUrl = '/images';
+
+module('Acceptances - Dataset', {
+  setup: function() {
+    App = startApp();
+    visit(indexUrl);
+  },
+
+  teardown: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+test("Shows the file-picker", function(){
+  expect(1);
+  var button = find('.ui-file-picker-button');
+  equal(button.length, 1, 'no file-picker button');
+});
