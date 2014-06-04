8d6a1c8ffe43b38cfb9be6693194d46c432d8b5b
Mattia Gheda <ghedamat@gmail.com>
2014-06-03 22:24:53 -0400

    notify every client in case of new images

--- client/app/controllers/images.js
+++ client/app/controllers/images.js
@@ -1,6 +1,29 @@
 export default Ember.ArrayController.extend({
   itemController: 'imageItem',
 
+  _subscribePusher: function(){
+    var _this = this;
+    var channel = this.pusher.subscribe('images');
+
+    function bindEvent(event) {
+      channel.bind(event, function(payload) {
+        var item = _this.findBy('id', payload.image_id);
+
+        if (item) {
+          Ember.sendEvent(item, event, [payload]);
+        } else if (event === 'processed') {
+          _this.insertAt(0, _this.store.find('image', payload.image_id));
+        }
+      });
+    }
+
+    this.set('channel', channel);
+
+    bindEvent('processing');
+    bindEvent('processed');
+    bindEvent('failed');
+  }.on('init'),
+
   actions: {
     addFiles: function(files) {
       files.forEach(function(file) {
--- client/app/initializers/pusher.js
+++ client/app/initializers/pusher.js
@@ -4,6 +4,6 @@ export default {
   initialize: function(container, app) {
     var pusher = new Pusher(ENV.PUSHER_APP_KEY);
     app.register('pusher:main', pusher, { instantiate: false });
-    app.inject('route', 'pusher', 'pusher:main');
+    app.inject('controller', 'pusher', 'pusher:main');
   }
 };
