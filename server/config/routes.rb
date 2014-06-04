31e0b9362faefa4ac838119075abdbdb27249143
Mattia Gheda <ghedamat@gmail.com>
2014-06-04 14:39:10 -0400

    add sidekiq web for demo

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,7 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
   namespace :api, defaults: { format: 'json' } do
     namespace :v1, path: '/', constraints: UploadServer::ApiConstraint.new(version: 1, default: true) do
       resources :images,  only: [:index, :show, :create]
