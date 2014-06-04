10d236b31f807d4f5df6fe5683d790eb47aed9da
Mattia Gheda <ghedamat@gmail.com>
2014-06-03 22:35:34 -0400

    reorder pusher code in controller

--- client/app/controllers/images.js
+++ client/app/controllers/images.js
@@ -1,27 +1,27 @@
 export default Ember.ArrayController.extend({
   itemController: 'imageItem',
 
-  _subscribePusher: function(){
+  bindEvent: function (event) {
     var _this = this;
+    this.get('channel').bind(event, function(payload) {
+      var item = _this.findBy('id', payload.image_id);
+
+      if (item) {
+        Ember.sendEvent(item, event, [payload]);
+      } else if (event === 'processed') {
+        _this.insertAt(0, _this.store.find('image', payload.image_id));
+      }
+    });
+  },
+
+  _subscribePusher: function(){
     var channel = this.pusher.subscribe('images');
-
-    function bindEvent(event) {
-      channel.bind(event, function(payload) {
-        var item = _this.findBy('id', payload.image_id);
-
-        if (item) {
-          Ember.sendEvent(item, event, [payload]);
-        } else if (event === 'processed') {
-          _this.insertAt(0, _this.store.find('image', payload.image_id));
-        }
-      });
-    }
 
     this.set('channel', channel);
 
-    bindEvent('processing');
-    bindEvent('processed');
-    bindEvent('failed');
+    this.bindEvent('processing');
+    this.bindEvent('processed');
+    this.bindEvent('failed');
   }.on('init'),
 
   actions: {
