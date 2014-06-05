1211e54b653072cc9018865b77b750f26f9267ae
Carsten Nielsen <heycarsten@gmail.com>
2014-06-04 20:11:41 -0400

    Just return latest 100 images.
    Ditching pagination for now since it’s outside the scope of the demo to
    do endless scrolling.

--- server/app/controllers/api/v1/images_controller.rb
+++ server/app/controllers/api/v1/images_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ImagesController < ApplicationController
   def index
-    render json: Image.processed.page(params[:page])
+    render json: Image.processed.newest.limit(100)
   end
 
   def show
