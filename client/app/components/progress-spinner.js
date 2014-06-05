1c81883856ed5cad514787582396923fc7fd6523
Carsten Nielsen <heycarsten@gmail.com>
2014-06-04 22:05:07 -0400

    Rename ui-spinner to progress-spinner.
    Also some mild twerking of style.

--- client/app/components/progress-spinner.js
+++ client/app/components/progress-spinner.js
@@ -19,9 +19,9 @@ var SPINNER_PROPS = [
 
 export default Ember.Component.extend({
   lines: 13,            // The number of lines to draw
-  length: 5,           // The length of each line
-  width: 5,            // The line thickness
-  radius: 10,           // The radius of the inner circle
+  length: 9,            // The length of each line
+  width: 4,             // The line thickness
+  radius: 11,           // The radius of the inner circle
   corners: 1,           // Corner roundness (0..1)
   rotate: 0,            // The rotation offset
   direction: 1,         // 1: clockwise, -1: counterclockwise
@@ -30,12 +30,12 @@ export default Ember.Component.extend({
   trail: 60,            // Afterglow percentage
   shadow: false,        // Whether to render a shadow
   hwaccel: false,       // Whether to use hardware acceleration
-  className: 'spinner', // The CSS class to assign to the spinner
+  className: 'progress-spinner', // The CSS class to assign to the spinner
   zIndex: 2e9,          // The z-index (defaults to 2000000000)
   top: '50%',           // Top position relative to parent
   left: '50%',          // Left position relative to parent
 
-  classNames: 'ui-spinner',
+  classNames: 'progress-spinner-container',
 
   didInsertElement: function() {
     var opts = this.getProperties(SPINNER_PROPS);
